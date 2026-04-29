901702b6d7338b594304f533226c4aa02158a8ac
Kapil Dev Pandey <bhupeshgamedev8777@gmail.com>
2026-04-29 13:15:14 +0530

    Initial commit - modular inventory system

---
+++ ADDONS/InventorySys/Scripts/InventoryManager.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name InventoryManager
+
+@export var max_inv_size : int
+@export var invetory : Array[]
---
+++ ADDONS/InventorySys/Scripts/ItemData.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name ItemData
+
+@export var item_name : String
+@export var stackable : bool
+@export var max_stackable_Amount : bool
+@export var item_type : String
+
+
+@export_category("UiStuff")
+@export var item_png : Texture2D
